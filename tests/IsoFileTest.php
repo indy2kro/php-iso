f71789aa055dbff4ba5b32018a9a9e0c0ed7edf9
Cristi Radu <indy2kro@gmail.com>
2025-01-12 14:29:09 +0200

    Update IsoFileTest.php

--- tests/IsoFileTest.php
+++ tests/IsoFileTest.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace PhpIso\Test;
 
 use PHPUnit\Framework\TestCase;
+use PhpIso\IsoFile;
 
 class IsoFileTest extends TestCase
 {
+    public function testConstructor(): void
+    {
+        $isoFile = new IsoFile();
+        $this->assertInstanceOf(IsoFile::class, $isoFile);
+    }
 }
